7d58ffd4b98cb80a6e943776162e0c0b855bef04
Andrea La Grotteria <lagrotteria.andrea@gmail.com>
2025-04-27 21:50:22 +0200

    adds getAnonymousID

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { MetaPixel } from 'meta-pixel-plugin';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    MetaPixel.echo({ value: inputValue })
-}
